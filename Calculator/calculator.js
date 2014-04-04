62b551ab5b7235a7ef4a6ab9866626c1df2631ba
DIGININJADEV <keiichi.lindley@gmail.com>
2014-04-03 21:02:31 -0600

    Added Calculator Code

---
+++ Calculator/calculator.js
@@ -0,0 +1,83 @@
+/* GLOBAL VARIABLES */
+var input = '';
+var result = '';
+var bOperatorPressed = false;
+var bOperatorFinished = true;
+var bInOperation = false;
+var keyPressCount = 0;
+var mathOperator = '';
+
+function calculate(num)
+{
+    var displayInput = document.getElementById("display");
+    var currentValue = displayInput.value;
+    num = input;
+    if(mathOperator == '*')
+    {
+        var calculatedResult = parseFloat(currentValue) * num;
+    }
+    if(mathOperator == '+')
+    {
+        var calculatedResult = parseFloat(currentValue) + parseFloat(num);
+    }
+    if(mathOperator == '/')
+    {
+        var calculatedResult = parseFloat(currentValue) / num;
+    }
+    if(mathOperator == '-')
+    {
+        var calculatedResult =  num - parseFloat(currentValue);
+    }
+    
+    var roundedResult = calculatedResult.toFixed(3);
+
+    
+    var displayInput = document.getElementById("display");
+    displayInput.value = roundedResult;
+    bOperatorPressed = false;
+    bOperatorFinished = false;
+    keyPressCount = 0;
+}
+
+
+
+function clearDisplay()
+{
+  var displayInput = document.getElementById("display").value = '0.';   
+}
+
+function turnOff()
+{
+  var displayInput = document.getElementById("display").value = '';    
+}
+
+function numPress(num)
+{
+    //input = input + ' ' + num;
+    var displayInput = document.getElementById("display");
+    var currentValue = displayInput.value;
+    if((currentValue != '0.')&&(bOperatorFinished == true))
+    {
+        displayInput.value = currentValue + num;
+    }else{
+        if(keyPressCount > 0)
+        {
+           displayInput.value = currentValue + num;
+        }else{
+           displayInput.value = num;
+            keyPressCount = keyPressCount + 1;
+        }
+        
+        
+    }
+}
+
+function arithmetic(type)
+{
+    bOperatorPressed = true;
+    bOperatorFinished = false;
+    mathOperator = type;
+    keyPressCount = 0;
+    var CurrentValue = document.getElementById("display").value;
+    input = CurrentValue;
+}
