45bb3a7afbbf52333febd97282e7f433493bfe94
DIGININJADEV <keiichi.lindley@gmail.com>
2014-04-08 21:36:46 -0600

    More Developed Data Structure
    Added basic data structure and handling

--- Recipes/recipes.js
+++ Recipes/recipes.js
@@ -1,12 +1,46 @@
 var recipes = [{
-  name: "",
-  yields: "",
-  author: "",
-  published: "",
-  instructions: "",
-  ingredients: "",
-  comments: ""
+  name: "Sam's Ragu",
+  yields: "8 servings",
+  author: "S Lindley",
+  published: "04/08/2014",
+  instructions: ["Dice 1 cup of yellow onions", "Dice 1.5 cups of carrots"],
+  ingredients: [{name: "salt", ammount: "1 tsb"}],
+  comments: "This is my first recipe"
 }];
+
+var recipe = recipes[0];
+
+for (var property in recipe) {
+  
+  var propertyValue = recipe[property]; 
+  
+  if((property === "instructions") || (property === "ingredients"))
+  {
+    if(property === "ingredients")
+    {
+      // Value is an array of objects
+      
+      var objIngredients = propertyValue[0];
+      for (var item in objIngredients) {
+        console.log(objIngredients[item]);
+      }
+    }else{
+      // Value is an array
+      
+      var arrInstructions = propertyValue; 
+      for(var i=0;i<arrInstructions.length;i++)
+      {
+        console.log(arrInstructions[i]); 
+      }
+    }
+  }else{
+    console.log(propertyValue);
+  }
+}
+
+// console.log(recipes[0].name);
+
+
 
 
 
