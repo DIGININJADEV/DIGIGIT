72ecde44bdd64257eb623c66b718a68991917b17
DIGININJADEV <keiichi.lindley@gmail.com>
2014-04-09 18:35:19 -0600

    Added some HTML/CSS to display recipes

--- Recipes/recipes.js
+++ Recipes/recipes.js
@@ -4,43 +4,66 @@ var recipes = [{
   author: "S Lindley",
   published: "04/08/2014",
   instructions: ["Dice 1 cup of yellow onions", "Dice 1.5 cups of carrots"],
-  ingredients: [{name: "salt", ammount: "1 tsb"}],
+  ingredients: [{name: "salt", ammount: "1 tsb"}, {name: "thyme", ammount: "1 tsb"}, {name: "basil", ammount: "1.5 tsb"}],
   comments: "This is my first recipe"
 }];
 
-var recipe = recipes[0];
+recipes.push({
+  name: "Sam's Pizza",
+  yields: "2 servings",
+  author: "S Lindley",
+  published: "04/09/2014",
+  instructions: ["Grate 1 cup of 4 cheese", "Slice tomoates"],
+  ingredients: [{name: "tomates", ammount: "1 cup"}, {name: "thyme", ammount: "1 tsb"}, {name: "basil", ammount: "1.5 tsb"}],
+  comments: "This is my second recipe"
+});
 
-for (var property in recipe) {
-  
-  var propertyValue = recipe[property]; 
-  
-  if((property === "instructions") || (property === "ingredients"))
-  {
-    if(property === "ingredients")
-    {
-      // Value is an array of objects
-      
-      var objIngredients = propertyValue[0];
-      for (var item in objIngredients) {
-        console.log(objIngredients[item]);
-      }
-    }else{
-      // Value is an array
-      
-      var arrInstructions = propertyValue; 
-      for(var i=0;i<arrInstructions.length;i++)
-      {
-        console.log(arrInstructions[i]); 
-      }
-    }
-  }else{
-    console.log(propertyValue);
-  }
+getRecipes(recipes);
+
+function getRecipes(recipes)
+{
+	for(var i=0;i<recipes.length;i++)
+	{
+		console.log(recipes[i].name); 
+	}
 }
 
-// console.log(recipes[0].name);
+function getRecipe(recipe)
+{
 
-
+	var recipe = recipe/*s[0]*/;
+	
+	for (var property in recipe) {
+		
+		var propertyValue = recipe[property]; 
+		
+		if((property === "instructions") || (property === "ingredients"))
+		{
+			if(property === "ingredients")
+			{
+				// Passed value is an array of objects
+			
+				var arrIngredients = propertyValue; 
+				for(var i=0;i<arrIngredients.length;i++)
+				{
+					var objIngredients = arrIngredients[i];
+					console.log(objIngredients.ammount + ' of ' + objIngredients.name);
+				}
+				
+			}else{
+				// Passed value is an array
+					
+				var arrInstructions = propertyValue; 
+				for(var i=0;i<arrInstructions.length;i++)
+				{
+					console.log('-- ' + arrInstructions[i]); 
+				}
+			}
+		}else{
+			console.log(propertyValue);
+		}
+	}
+}
 
 
 
